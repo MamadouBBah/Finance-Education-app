dec94566f7c051867b9d5863cdb2a316014843a6
Mamadou B Bah <97556256+MamadouBBah@users.noreply.github.com>
2026-04-20 20:51:17 -0600

    Update App.js

--- App.js
+++ App.js
@@ -204,7 +204,7 @@ function Header() {
           </span>
 
           <a
-            href=" https://mamadoubbah.github.io/"
+            href="https://mamadoubbah.github.io/"
             style={{
               fontFamily: "'DM Sans', sans-serif",
               fontSize: "0.78rem",
